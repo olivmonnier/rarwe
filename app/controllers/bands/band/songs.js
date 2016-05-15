8c973514b24fdbe4c4ecb787d16d6382a5921ef1
olivmonnier <omonnier.dev@gmail.com>
2016-05-15 23:59:11 +0200

    p217

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -9,6 +9,26 @@ export default Ember.Controller.extend({
   canCreateSong: Ember.computed('songCreationStarted', 'model.songs.length', function () {
     return this.get('songCreationStarted') || this.get('model.songs.length');
   }),
+  sortBy: 'ratingDesc',
+  sortProperties: Ember.computed('sortBy', function () {
+    var options = {
+      'ratingDesc': 'rating:desc,title:asc',
+      'ratingAsc': 'rating:asc,title:asc',
+      'titleDesc': 'title:desc',
+      'titleAsc': 'title:asc'
+    };
+
+    return options[this.get('sortBy')].split(',');
+  }),
+  sortedSongs: Ember.computed.sort('matchingSongs', 'sortProperties'),
+  searchTerm: '',
+  matchingSongs: Ember.computed('model.songs.@each.title', 'searchTerm', function () {
+    var searchTerm = this.get('searchTerm').toLowerCase();
+
+    return this.get('model.songs').filter(function (song) {
+      return song.get('title').toLowerCase().indexOf(searchTerm) !== -1;
+    });
+  }),
   actions: {
     enableSongCreation: function () {
       this.set('songCreationStarted', true);
@@ -22,6 +42,9 @@ export default Ember.Controller.extend({
       }
       song.set('rating', rating);
       return song.save();
+    },
+    setSorting: function (option) {
+      this.set('sortBy', option);
     }
   }
 });
